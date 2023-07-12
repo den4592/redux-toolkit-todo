4e19aac51ee6c4d3bdfecbf8f85eb73e3cf91999
den4592 <den4592@naver.com>
2023-07-12 14:19:51 +0900

    todoList 컴포넌트 로직 및 스타일링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="w-full h-screen flex items-center content-center min-h-[700px]">
+    <div className="w-full h-screen flex items-center justify-center flex-col min-h-[700px]">
       <TodoForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,33 @@
+import { selectAllTodos } from "../features/todos/todosSlice";
+import { useSelector } from "react-redux";
+
+const TodoList = () => {
+  const todos = useSelector(selectAllTodos);
+
+  return (
+    <div className="container text-left">
+      <h3 className="text-white text-base mt-10 mb-10 ">
+        Count: {todos.length}
+      </h3>
+      {todos &&
+        todos.map((todo) => (
+          <div className="w-full h-[5rem] p-5 rounded bg-white flex items-center justify-between ">
+            <p className="text-base">{todo.text}</p>
+            <div className="flex gap-5">
+              <button className="shrink-0 p-2 border-2 border-yellow-400  rounded hover:text-white hover:bg-yellow-400">
+                edit
+              </button>
+              <button className="shrink-0 p-2 border-2 border-green-400  rounded hover:text-white hover:bg-green-400">
+                complete
+              </button>
+              <button className="shrink-0 p-2 border-2 border-red-400  rounded hover:text-white hover:bg-red-400">
+                remove
+              </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default TodoList;
